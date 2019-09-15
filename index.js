733bbe36a2cb16e825e61c48c5a4688ac4163003
Diego Pereira <dhiego_ti@hotmail.com>
2019-09-14 23:02:26 -0300

    add quotation bcb

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const convert = require('./lib/convert')
+const apiBCB = require('./lib/api-bcb.js')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) =>{
-    res.render('home')
+app.get('/', async(req, res) =>{
+    const cotacao = await apiBCB.getCotacao()
+    res.render('home', {
+        cotacao
+    })
 })
 app.get('/quotation', (req, res) => {
     const { quotation, amount } = req.query
